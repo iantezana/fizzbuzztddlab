2cea60a530b7f352ccd23b9741046695ceb72052
iantezana <84368088+iantezana@users.noreply.github.com>
2025-04-12 08:49:28 +0000

    Se anade caso buzz SIN PRUEBAS

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,11 @@ function fizzbuzz(number) {
     if (number % 3 === 0) {
         return "Fizz";
     }
+
+    if (number % 5 === 0) {
+        return "Bu";
+    }
+ 
     return number.toString();
 }
 
