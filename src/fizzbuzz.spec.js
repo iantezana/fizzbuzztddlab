55220d4e9431aab07688679f7e0d12c85b3872d3
iantezana <84368088+iantezana@users.noreply.github.com>
2025-04-12 08:45:30 +0000

    se crea prueba para el caso de otros multiplos de 3

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,4 +4,8 @@ describe("FizzBuzz", () => {
     it("debería devolver 'Fizz' si el número es divisible por 3", () => {
         expect(fizzbuzz(3)).toEqual("Fizz");
     });
+
+    it("debería devolver 'Fizz' si el número es divisible por 3 con otros casos", () => {
+        expect(fizzbuzz(6)).toEqual("Fizz");
+    });
 })
